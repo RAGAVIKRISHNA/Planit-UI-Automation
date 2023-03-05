134b7084775d1e32741baaaa808935b440f0609f
Ragavi Muthukrishnan <68452571+RAGAVIKRISHNA@users.noreply.github.com>
2023-03-05 12:34:53 +1100

    Update Hook.java

--- src/main/java/com/planit/shoptoys/utility/Hook.java
+++ src/main/java/com/planit/shoptoys/utility/Hook.java
@@ -12,7 +12,7 @@ public class Hook {
 
 	public static WebDriver getDriver()
 	{
-		System.setProperty(“webdriver.chrome.driver”, chromedriver”);
+		System.setProperty("webdriver.chrome.driver", chromedriver);
 		driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
